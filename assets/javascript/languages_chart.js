63fbf09b2c19413e98909719c5c4e7be42212e70
Maxime LEGENDRE <mlegendr@e2r8p15.42.fr>
2017-07-09 13:21:41 +0200

    Add languages chart

--- assets/javascript/languages_chart.js
+++ assets/javascript/languages_chart.js
@@ -1,10 +1,10 @@
 
 google.charts.load('current', { 'packages': ['corechart'] });
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawMainLChart);
+google.charts.setOnLoadCallback(drawSecLChart);
 
-function drawChart() {
-
-    var data = google.visualization.arrayToDataTable([
+function drawMainLChart() {
+    var dataArray = [
         ['Language', 'days'],
         ['Ruby', 105],
         ['Python', 105],
@@ -13,14 +13,39 @@ function drawChart() {
         ['C++', 251],
         ['C#', 32],
         ['PHP', 124]
-    ]);
-
+    ];
+    var data = google.visualization.arrayToDataTable(dataArray);
     var options = {
-        title: 'My languages experience',
+        title: 'Main language used in projects',
         pieHole: 0.5,
     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('languages-chart'));
+    var chart = new google.visualization.PieChart(document.getElementById('main-languages-chart'));
+    var totalDays = 0;
+    for (var i = 1; i < dataArray.length; i++) {
+        totalDays += dataArray[i][1];
+    }
+    $('#cnt-main-languages').html('<b>' + Math.trunc(totalDays / 365) + '</b> years<br/>' + '<b>' + Math.trunc((totalDays % 365) / 30) + '</b> months<br/>' + '<b>' + (totalDays % 365) % 30 + '</b> days')
+    chart.draw(data, options);
+}
 
+function drawSecLChart() {
+    var dataArray = [
+        ['Language', 'days'],
+        ['none', 623],
+        ['javasript', 100],
+        ['SQL', 82],
+        ['html/css', 100]
+    ];
+    var data = google.visualization.arrayToDataTable(dataArray);
+    var options = {
+        title: 'secondary language used in projects',
+        pieHole: 0.5,
+        slices: {
+            0: { color: '#d3d3d3' }
+          }
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('secondary-languages-chart'));
     chart.draw(data, options);
 }
