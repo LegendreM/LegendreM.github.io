7e0dd75eb86b4aba01388f6bcf3e30c35ddae08a
Maxime LEGENDRE <mlegendr@e6r3p1.vp.42.fr>
2017-07-13 17:36:10 +0200

    Change tab activation on click

--- assets/javascript/index_vue.js
+++ assets/javascript/index_vue.js
@@ -1,21 +1,29 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    langData: langData,
-    frameworkData: frameworkData,
-    dbData: dbData,
-    categoryData: categoryData,
-    projectsData: projectsData,
-    panelTabs: [{name: "All", isActive: true}, {name: "Public", isActive: false}, {name: "Private", isActive: false}]
-  },
-  mounted: function () {
-    google.charts.load('current', { 'packages': ['corechart'] });
-    google.charts.load('current', {'packages':['sankey']});
-    google.charts.setOnLoadCallback(drawMainLChart);
-    google.charts.setOnLoadCallback(drawSecLChart);
-    google.charts.setOnLoadCallback(drawFrameworkChart);
-    google.charts.setOnLoadCallback(drawDBChart);
-    google.charts.setOnLoadCallback(drawCategoryChart);
-    google.charts.setOnLoadCallback(drawCategorySankeyChart);
-  }
+    el: '#app',
+    data: {
+        langData: langData,
+        frameworkData: frameworkData,
+        dbData: dbData,
+        categoryData: categoryData,
+        projectsData: projectsData,
+        panelTabs: [{name: "All", isActive: true}, {name: "Public", isActive: false}, {name: "Private", isActive: false}]
+    },
+    mounted: function () {
+        google.charts.load('current', { 'packages': ['corechart'] });
+        google.charts.load('current', {'packages':['sankey']});
+        google.charts.setOnLoadCallback(drawMainLChart);
+        google.charts.setOnLoadCallback(drawSecLChart);
+        google.charts.setOnLoadCallback(drawFrameworkChart);
+        google.charts.setOnLoadCallback(drawDBChart);
+        google.charts.setOnLoadCallback(drawCategoryChart);
+        google.charts.setOnLoadCallback(drawCategorySankeyChart);
+    },
+    methods: {
+        clickTab: function (event) {
+            for (var i = 0; i < this.panelTabs.length; i++) {
+                this.panelTabs[i].isActive = false;
+            }
+            this.panelTabs[event.target.id].isActive = true;
+        }
+    }
 })
